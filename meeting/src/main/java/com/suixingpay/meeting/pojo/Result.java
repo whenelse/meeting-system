232e0deb77348d3961e2e996250fde047c061325
whenelse <2547407062@qq.com>
2019-12-18 15:10:50 +0800

    新增返回类

--- meeting/src/main/java/com/suixingpay/meeting/pojo/Result.java
+++ meeting/src/main/java/com/suixingpay/meeting/pojo/Result.java
@@ -1,9 +1,7 @@
-package com.suixingpay.seckill.pojo;
-
+package com.suixingpay.meeting.pojo;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import org.springframework.stereotype.Component;
 
 /**
